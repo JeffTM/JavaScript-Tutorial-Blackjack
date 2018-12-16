fa03a6ca7a671f10574269eac792038be6530318
Jeff Matthews <jeffmtthws@gmail.com>
2018-12-15 20:22:25 -0500

    Added generate deck function

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ function setStatus(text)
 	document.getElementById('status').innerHTML = text;
 }
 
+function generateDeck()
+{
+	let suits = ['Clubs', 'Spades', 'Diamonds', 'Hearts'];
+	let ranks = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace'];
+	let cards = []
+	for (i = 0; i < suits.length; ++i)
+	{
+		for (j = 0; j < ranks.length; ++j)
+		{
+			cards.push(ranks[j] + ' of ' + suits[i]);
+		}
+	}
+	return cards;
+}
+
 function main()
 {
+	let deck = generateDeck();
 	setStatus('Game loaded. Welcome to Blackjack!');
+	setStatus(deck);
 }
