d67c49e4562c420a01cc682d19d8e09054336915
Jeff Matthews <jeffmtthws@gmail.com>
2018-12-22 18:01:27 -0500

    Further work on Deck class

--- deck.js
+++ deck.js
@@ -1,20 +1,39 @@
-//A deck of Card objects
+//A deck of Card objects for blackjack
 //Requires cards.js to be loaded first
 
 //Creates and empty deck. If passed true it will load a standard deck using makeStandardDeck()
 function Deck(loadStandardDeck = false)
 {
+	//note that this array is used as a stack. The first card is at the last index
 	this.cards = [];
 	if (loadStandardDeck)
 		this.makeStandardDeck();
 }
 
+Deck.prototype.clear = function()
+{
+	this.cards = [];
+}
+
+//Gets the card at index but does not remove it
+Deck.prototype.get = function(index)
+{
+	return this.cards[this.cards.length - index - 1];
+}
+
+//Returns the top card on the deck but does not remove it
+Deck.prototype.getLast = function()
+{
+	return this.cards[this.cards.length - 1];
+}
+
+//Makes a standard blackjack deck in a specific order
 Deck.prototype.makeStandardDeck = function()
 {
+	this.clear();
 	let suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
 	let ranks = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
 	let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
-	this.cards = [];
 	for (i = 0; i < suits.length; ++i)
 	{
 		for (j = 0; j < ranks.length; ++j)
@@ -22,4 +41,21 @@ Deck.prototype.makeStandardDeck = function()
 			this.cards.push(new Card(ranks[j], suits[i], values[j]));
 		}
 	}
+}
+
+//Removes the top card from the deck and returns it
+Deck.prototype.pop = function()
+{
+	return this.cards.pop();
+}
+
+Deck.prototype.push = function(card)
+{
+	this.cards.push(card);
+}
+
+//Shuffles the deck
+Deck.prototype.shuffle = function()
+{
+	//ToDo
 }
